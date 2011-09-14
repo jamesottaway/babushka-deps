8483167785687735a588abe24abc6761c309c1b3
James Ottaway <james@ottaway.mp>
2011-09-15 09:00:12 +1000

    SABnzbd

---
+++ sabnzbd.rb
@@ -0,0 +1,7 @@
+dep 'SABnzbd.app' do
+  source 'http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/sabnzbd-0.6.9/SABnzbd-0.6.9-osx-lion.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsabnzbdplus%2Ffiles%2Fsabnzbdplus%2Fsabnzbd-0.6.9%2F&ts=1316039666&use_mirror=internode'
+end
+
+dep 'SABnzbd.app', :for => :snow_leopard do
+  source 'http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/sabnzbd-0.6.9/SABnzbd-0.6.9-osx.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsabnzbdplus%2Ffiles%2Fsabnzbdplus%2Fsabnzbd-0.6.9%2F&ts=1316039619&use_mirror=internode'
+end
