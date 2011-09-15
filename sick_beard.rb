950c017e135b5a3138e3d43de4fa435a69ab9c23
James Ottaway <james@ottaway.mp>
2011-09-15 13:13:14 +1000

    Cheetah

--- sick_beard.rb
+++ sick_beard.rb
@@ -1,4 +1,19 @@
+dep 'cheetah' do
+  met? { '/usr/local/bin/cheetah'.to_fancypath.exists? }
+  meet {
+    handle_source 'http://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz#md5=853917116e731afbc8c8a43c37e6ddba' do |tarball|
+      in_build_dir do |build_dir|
+        cheetah_dir = (build_dir / tarball.path.basename).children.first
+        cd cheetah_dir do
+          sudo 'python setup.py install'
+        end
+      end
+    end
+  }
+end
+
 dep 'SickBeard' do
+  requires 'cheetah'
   destination = '/Applications'.to_fancypath / name
   met? { destination.exists? && !destination.empty? }
   meet {
