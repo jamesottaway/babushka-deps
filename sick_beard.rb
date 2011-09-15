d902d9a313e482e85c49e9dfe674dfd18ca0599e
James Ottaway <james@ottaway.mp>
2011-09-15 15:02:52 +1000

    Sick Beard LaunchAgent

--- sick_beard.rb
+++ sick_beard.rb
@@ -25,3 +25,18 @@ dep 'SickBeard' do
     end
   }
 end
+
+dep 'SickBeard launchagent' do
+  requires 'SickBeard'
+  define_var :sickbeard_home, :default => '/Applications/SickBeard', :message => 'Where does Sick Beard live?'
+  config = 'https://raw.github.com/gist/aa2d7431902f39803524/303c5bd3713b9ffec537834ef598481f0c39baac/com.sickbeard.sickbeard.plist'
+  launch_agents = '~/Library/LaunchAgents'.to_fancypath
+  plist = launch_agents / 'com.sickbeard.sickbeard.plist'
+  met? { plist.exists? && plist.read[var :sickbeard_home] }
+  meet {
+    Babushka::Resource.get config do |conf|
+      conf.copy plist
+    end
+    shell "sed -i '' 's~$SICKBEARD_HOME~#{var :sickbeard_home}~g' #{plist}"
+  }
+end
