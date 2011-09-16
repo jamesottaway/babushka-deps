c5837e7ca3731258df71cbb4b53f07ceba8b06e6
James Ottaway <james@ottaway.mp>
2011-09-16 11:42:45 +1000

    Couch Potato should set the retention too

--- couch_potato.rb
+++ couch_potato.rb
@@ -40,10 +40,11 @@ dep 'CouchPotato-config.template' do
   define_var :plex_movies_dir, :message => 'Where does Plex scan for movies?'
   define_var :nzbmatrix_username, :message => 'NZBMatrix Username'
   define_var :nzbmatrix_api_key, :message => 'NZBMatrix API Key'
+  define_var :usenet_retention, :message => 'Usenet Retention'
   define_var :twitter_username_token, :message => 'Twitter Username Token'
   define_var :twitter_password_token, :message => 'Twitter Password Token'
   
-  source 'https://gist.github.com/raw/80ac401612dd56db2317/f75d628fe712f59dbd8c66a6ed22f4a9578e6344/couchpotato.ini'
+  source 'https://gist.github.com/raw/80ac401612dd56db2317/cc9286d2bf033dd64480878d33ba254926a2baec/couchpotato.ini'
   destination '/Applications/CouchPotato/config.ini'
   
   arguments ({
@@ -59,6 +60,7 @@ dep 'CouchPotato-config.template' do
     '$PLEX_MOVIES_DIR' => var(:plex_movies_dir),
     '$NZBMATRIX_USERNAME' => var(:nzbmatrix_username),
     '$NZBMATRIX_API_KEY' => var(:nzbmatrix_api_key),
+    '$USENET_RETENTION' => var(:usenet_retention),
     '$TWITTER_USERNAME_TOKEN' => var(:twitter_username_token),
     '$TWITTER_PASSWORD_TOKEN' => var(:twitter_password_token)
     })
