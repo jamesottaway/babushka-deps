e47c1c29dae94964d488a01565a61360c9450311
James Ottaway <james@ottaway.mp>
2011-09-16 11:42:04 +1000

    Added a few more flags to the SABnzbd config

--- sabnzbd.rb
+++ sabnzbd.rb
@@ -24,32 +24,45 @@ end
 
 dep 'SABnzbd-config.template' do
   requires 'SABnzbd.app'
+  
   define_var :sabnzbd_username, :message => 'SABnzbd Username', :default => 'admin'
   define_var :sabnzbd_password, :message => 'SABnzbd Password'
   define_var :sabnzbd_api_key, :message => 'SABnzbd API Key', :default => SecureRandom.hex
   define_var :sabnzbd_nzb_key, :message => 'SABnzbd NZB Key', :default => SecureRandom.hex
+  define_var :sabnzbd_http_port, :message => 'SABnzbd HTTP Port', :default => '88080'
+  define_var :sabnzbd_https_port, :message => 'SABnzbd HTTPS Port', :default => '9090'
+  define_var :sabnzbd_pending_downloads_dir, :message => 'SABnzbd Pending Downloads Dir'
+  define_var :sabnzbd_complete_downloads_dir, :message => 'SABnzbd Complete Downloads Dir'
   define_var :nzbmatrix_username, :message => 'NZBMatrix Username'
   define_var :nzbmatrix_api_key, :message => 'NZBMatrix API Key'
   define_var :usenet_host, :message => 'Usenet Host'
   define_var :usenet_port, :message => 'Usenet Port'
   define_var :usenet_username, :message => 'Usenet Username'
   define_var :usenet_password, :message => 'Usenet Password'
-  define_var :usenet_num_connections, :message => 'Usenet Number of Connection', :default => '20'
+  define_var :usenet_connections_limit, :message => 'Usenet Connections Limit', :default => '20'
   define_var :usenet_use_ssl, :message => 'Usenet SSL?', :default => '1'
+  define_var :usenet_retention, :message => 'Usenet Retention'
+  
   source 'https://gist.github.com/raw/125aa788d32f14ecad61/3a09ccc5398f0d319084be68e554f00587886248/sabnzbd.ini'
   destination '~/Library/Application Support/SABnzbd/sabnzbd.ini'
+  
   arguments ({
     '$SABNZBD_USERNAME' => var(:sabnzbd_username),
     '$SABNZBD_PASSWORD' => var(:sabnzbd_password),
     '$SABNZBD_API_KEY' => var(:sabnzbd_api_key),
     '$SABNZBD_NZB_KEY' => var(:sabnzbd_nzb_key),
+    '$SABNZBD_HTTP_PORT' => var(:sabnzbd_http_port),
+    '$SABNZBD_HTTPS_PORT' => var(:sabnzbd_https_port),
+    '$SABNZBD_PENDING_DOWNLOADS_DIR' => var(:sabnzbd_pending_downloads_dir),
+    '$SABNZBD_COMPLETE_DOWNLOADS_DIR' => var(:sabnzbd_complete_downloads_dir),
     '$NZBMATRIX_USERNAME' => var(:nzbmatrix_username),
     '$NZBMATRIX_API_KEY' => var(:nzbmatrix_api_key),
     '$USENET_HOST' => var(:usenet_host),
     '$USENET_PORT' => var(:usenet_port),
     '$USENET_USERNAME' => var(:usenet_username),
     '$USENET_PASSWORD' => var(:usenet_password),
-    '$USENET_NUM_CONNECTIONS' => var(:usenet_num_connections),
-    '$USENET_USE_SSL' => var(:usenet_use_ssl)
+    '$USENET_CONNECTIONS_LIMIT' => var(:usenet_connections_limit),
+    '$USENET_USE_SSL' => var(:usenet_use_ssl),
+    '$USENET_RETENTION' => var(:usenet_retention)
     })
 end
