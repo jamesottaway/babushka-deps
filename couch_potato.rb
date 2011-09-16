20ae0820e196bf684c7b1c6ce73645d282bd5fba
James Ottaway <james@ottaway.mp>
2011-09-16 11:42:22 +1000

    Added a few more flags to the Couch Potato config

--- couch_potato.rb
+++ couch_potato.rb
@@ -27,30 +27,35 @@ end
 
 dep 'CouchPotato-config.template' do
   requires 'CouchPotato'
+  
   define_var :couchpotato_username, :message => 'Couch Potato Username', :default => 'admin'
   define_var :couchpotato_password, :message => 'Couch Potato Password'
-  define_var :sabnzbd_username, :message => 'SABnzbd Username'
+  define_var :couchpotato_http_port, :message => 'Couch Potato HTTP Port', :default => '5000'
+  define_var :sabnzbd_username, :message => 'SABnzbd Username', :default => 'admin'
   define_var :sabnzbd_password, :message => 'SABnzbd Password'
   define_var :sabnzbd_host, :message => 'SABnzbd Host', :default => 'localhost'
   define_var :sabnzbd_port, :message => 'SABnzbd Port', :default => '8080'
   define_var :sabnzbd_api_key, :message => 'SABnzbd API Key'
-  define_var :sabnzbd_movies_download_dir, :message => 'Where does SABnzbd store completed movie downloads?'
+  define_var :sabnzbd_complete_downloads_dir, :message => 'Where does SABnzbd store completed downloads?'
   define_var :plex_movies_dir, :message => 'Where does Plex scan for movies?'
   define_var :nzbmatrix_username, :message => 'NZBMatrix Username'
   define_var :nzbmatrix_api_key, :message => 'NZBMatrix API Key'
   define_var :twitter_username_token, :message => 'Twitter Username Token'
   define_var :twitter_password_token, :message => 'Twitter Password Token'
-  source 'https://gist.github.com/raw/80ac401612dd56db2317/236af050e9f62861fe3283b84c15bf1b34337a65/couchpotato.ini'
+  
+  source 'https://gist.github.com/raw/80ac401612dd56db2317/f75d628fe712f59dbd8c66a6ed22f4a9578e6344/couchpotato.ini'
   destination '/Applications/CouchPotato/config.ini'
+  
   arguments ({
     '$COUCHPOTATO_USERNAME' => var(:couchpotato_username),
     '$COUCHPOTATO_PASSWORD' => var(:couchpotato_password),
+    '$COUCHPOTATO_HTTP_PORT' => var(:couchpotato_http_port),
     '$SABNZBD_USERNAME' => var(:sabnzbd_username),
     '$SABNZBD_PASSWORD' => var(:sabnzbd_password),
     '$SABNZBD_HOST' => var(:sabnzbd_host),
     '$SABNZBD_PORT' => var(:sabnzbd_port),
     '$SABNZBD_API_KEY' => var(:sabnzbd_api_key),
-    '$SABNZBD_MOVIES_DOWNLOAD_DIR' => var(:sabnzbd_movies_download_dir),
+    '$SABNZBD_COMPLETE_DOWNLOADS_DIR' => var(:sabnzbd_complete_downloads_dir),
     '$PLEX_MOVIES_DIR' => var(:plex_movies_dir),
     '$NZBMATRIX_USERNAME' => var(:nzbmatrix_username),
     '$NZBMATRIX_API_KEY' => var(:nzbmatrix_api_key),
