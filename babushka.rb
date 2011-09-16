1084f251f86e6516cdf41f32eac5a0424721d841
James Ottaway <james@ottaway.mp>
2011-09-16 22:22:51 +1000

    Let's keep them deps up and with it

--- babushka.rb
+++ babushka.rb
@@ -5,3 +5,9 @@ end
 dep 'downloads.clean' do
   clean in_download_dir {|path| path}
 end
+
+dep 'deps up-to-date' do
+  repo = Babushka::GitRepo.new '~/.babushka/deps'
+  met? { repo.clean? or unmeetable("There are local changes in #{repo.path}.") }
+  meet { repo.reset_hard! 'origin/master' }
+end
