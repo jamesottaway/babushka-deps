aa25909e03fbd52dd62c0a3ff536d1c5eea1da6b
James Ottaway <james@ottaway.mp>
2011-09-16 22:23:10 +1000

    Fixed launchctl meta

--- templates/launchctl.rb
+++ templates/launchctl.rb
@@ -3,6 +3,6 @@ meta :launchctl do
   accepts_value_for :plist
   template {
     met? { sudo('launchctl list')[label] }
-    meet { sudo "launchctl load #{plist}" }
+    meet { sudo "launchctl load -w #{plist}" }
   }
 end
