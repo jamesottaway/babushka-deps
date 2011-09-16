cb0d4fecf284b1a57eea063e27126bf0fb2aa423
James Ottaway <james@ottaway.mp>
2011-09-16 21:50:23 +1000

    Media Center dep

---
+++ machines/media_center.rb
@@ -0,0 +1,6 @@
+dep 'Media Center' do
+  requires 'RemoteLogin', 'ScreenSharing',
+  'dot files', 'OS X Defaults', 'ZSH',
+  'Alfred.app', 'GoogleChrome.app', 'Plex.app',
+  'SickBeard', 'CouchPotato'
+end
