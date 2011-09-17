a5ff7a568d3038d016ef58310ae80d313ebed576
James Ottaway <james@ottaway.mp>
2011-09-17 10:22:27 +1000

    Make sure the template's destination's parent exists

--- templates/template.rb
+++ templates/template.rb
@@ -9,6 +9,7 @@ meta :template do
       @settings = arguments.map(&:last)
     }
     met? { @destination.exists? && @settings.all? { |setting| @destination.read[setting] } }
+    before { @destination.parent.mkdir unless @destination.parent.exists? }
     meet {
       Babushka::Resource.get template do |file|
         file.copy @destination
