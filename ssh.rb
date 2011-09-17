0930eb5623089247c9b43e0bb4d7b771447114f7
James Ottaway <james@ottaway.mp>
2011-09-17 17:52:45 +1000

    Fixed up the 'ssh keys' dep

--- ssh.rb
+++ ssh.rb
@@ -3,6 +3,10 @@ dep 'ssh keys' do
   public_key = ssh_dir / 'id_rsa.pub'
   private_key = ssh_dir / 'id_rsa'
   met? { public_key.exists? && private_key.exists? }
-  prepare { define_var :email, :message => 'What is your email address?' }
-  meet { shell "ssh-keygen -t rsa -C #{var :email}" }
+  prepare {
+    define_var :email, :message => 'What is your email address?'
+    define_var :ssh_passphrase, :message => 'What passphrase do you want to encrypt your SSH keys with?'
+    define_var :ssh_path, :message => 'Where should we store your SSH keys?', :default => '~/.ssh'
+  }
+  meet { shell "ssh-keygen -t rsa -N #{var :ssh_passphrase} -C #{var :email} -f #{var :ssh_path}/id_rsa" }
 end
