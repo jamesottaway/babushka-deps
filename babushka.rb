613eaefc3676e8ddee35f9a8fc766a7f8b4f9f8d
James Ottaway <james@ottaway.mp>
2011-09-17 10:58:41 +1000

    I give up on that idea

--- babushka.rb
+++ babushka.rb
@@ -5,11 +5,3 @@ end
 dep 'downloads.clean' do
   clean in_download_dir {|path| path}
 end
-
-dep 'deps up-to-date' do
-  repo = Babushka::GitRepo.new '~/.babushka/deps'
-  setup { !repo.ahead? or unmeetable("There are local changes in #{repo.path}.") }
-  met? { !repo.behind? }
-  before { repo.repo_shell 'git fetch origin' }
-  meet { repo.reset_hard! 'origin/master' }
-end
