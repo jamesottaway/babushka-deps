ce3f3a0d0a13631fdc17f6a01f0a927ac4bb5ab0
James Ottaway <james@ottaway.mp>
2011-09-20 17:16:16 +1000

    Should make the 'dot files' dep easier to read

--- dot_files.rb
+++ dot_files.rb
@@ -6,35 +6,33 @@ end
 dep 'dot files symlinked' do
   requires 'dot files.cloned'
   
-  def home_dir
-    '~'.to_fancypath
-  end
-  
   def dot_files
-    (home_dir / '.dot-files').all_children
+    '~/.dot-files'.to_fancypath.all_children.reject { |dot_file| dot_file.basename == '.git' }.map { |dot_file| DotFile.new dot_file }
   end
   
   def missing_dot_files
-    dot_files.select { |d|
-      (home_dir / d.basename).readlink != d
-    }.reject { |d| d.basename == '.git' }
+    dot_files.select { |dot_file| dot_file.destination.readlink != dot_file.source }
+  end
+  
+  def pre_symlink_cleanup
+    missing_dot_files.map(&:destination).select { |destination| destination.exists? }.map { |destination| destination.remove }
   end
   
   met? { missing_dot_files.empty? }
-  before {
-    missing_dot_files.map { |d|
-      home_dir / d.basename
-    }.select { |d|
-      d.exists?
-    }.map { |d|
-      d.remove
-    }
-  }
-  meet {
-    missing_dot_files.map { |d| shell "ln -s #{d} #{home_dir}" }
-  }
+  before { pre_symlink_cleanup }
+  meet { missing_dot_files.map { |d| shell "ln -s #{d.source} #{d.destination}" } }
 end
 
 dep 'dot files' do
   requires 'dot files symlinked'
+end
+
+class DotFile < Struct.new(:source, :destination)
+  def initialize source
+    self.source, self.destination = source, to_destination(source)
+  end
+  
+  def to_destination source
+    '~'.to_fancypath / source.basename.to_s.gsub('@', '/')
+  end
 end
