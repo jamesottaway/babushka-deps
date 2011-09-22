34ef999e4aa6e81ad712e7a1ab8a98c55f7918cc
James Ottaway <james@ottaway.mp>
2011-09-22 15:38:37 +1000

    Refactored my SSH keys dep

--- ssh.rb
+++ ssh.rb
@@ -1,12 +1,6 @@
 dep 'ssh keys' do
-  ssh_dir = '~/.ssh'.to_fancypath
-  public_key = ssh_dir / 'id_rsa.pub'
-  private_key = ssh_dir / 'id_rsa'
-  met? { public_key.exists? && private_key.exists? }
-  prepare {
-    define_var :email, :message => 'What is your email address?'
-    define_var :ssh_passphrase, :message => 'What passphrase do you want to encrypt your SSH keys with?'
-    define_var :ssh_path, :message => 'Where should we store your SSH keys?', :default => '~/.ssh'
-  }
-  meet { shell "ssh-keygen -t rsa -N #{var :ssh_passphrase} -C #{var :email} -f #{var :ssh_path}/id_rsa" }
+  setup { define_var :ssh_dir, :message => 'Where do your SSH keys live? ', :default => '~/.ssh' }
+  met? { (var(:ssh_dir) / 'id_rsa.pub').exists? && (var(:ssh_dir) / 'id_rsa').exists? }
+  prepare { define_var :ssh_password, :message => 'What passphrase do you want to encrypt your SSH keys with?' }
+  meet { shell "ssh-keygen -t rsa -N #{var :ssh_password} -f #{var :ssh_dir}/id_rsa" }
 end
