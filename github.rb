7726cc3a92597bf47b41d22e3152717730bce82b
James Ottaway <james@ottaway.mp>
2011-09-22 17:39:33 +1000

    Refactored GitHub stuff

--- github.rb
+++ github.rb
@@ -1,10 +1,16 @@
 dep 'github has my public key' do
   requires 'ssh keys'
-  github_api = 'https://api.github.com'
-  define_var :github_username, :message => 'What is your GitHub username?', :default => 'jamesottaway'
+  define_var :github_username, :message => 'What is your GitHub username?'
   define_var :github_password, :message => 'What is your GitHub password?'
-  set :public_key, shell('cat ~/.ssh/id_rsa.pub')
   met? { raw_shell('ssh -T git@github.com 2>&1').stdout['successfully authenticated'] }
-  prepare { set :hostname, shell('hostname') }
-  meet { shell "curl -u '#{var :github_username}:#{var :github_password}' -d '{\"title\": \"#{var :hostname}\", \"key\": \"#{var :public_key}\"}' #{github_api}/user/keys" }
+  prepare {
+    set :hostname, shell('hostname')
+    set :public_key, shell('cat ~/.ssh/id_rsa.pub')
+    set :github_api, 'https://api.github.com'
+  }
+  meet {
+    auth = "#{var :github_username}:#{var :github_password}"
+    args = "{\"title\": \"#{var :hostname}\", \"key\": \"#{var :public_key}\"}"
+    shell "curl -u '#{auth}' -d '#{args}' #{var :github_api}/user/keys"
+  }
 end
